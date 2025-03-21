2dd267f114d225c3d7419c5411a59506be7aa76e
Baltinha <matheusramoss666@gmail.com>
2025-03-21 16:17:10 -0300

    imagens temporárias na tela

--- Test-CandyCrushGame/Assets/Scripts/BackgroundTile.cs
+++ Test-CandyCrushGame/Assets/Scripts/BackgroundTile.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BackgroundTile : MonoBehaviour
 {
+    public GameObject[] Dots;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Iniztialize();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Iniztialize() 
+    {
+        int dotToUse = Random.Range(0, Dots.Length);
+        GameObject dot = Instantiate(Dots[dotToUse], transform.position, Quaternion.identity);
+        dot.transform.parent = this.transform;
+        dot.name = this.gameObject.name;
     }
 }
--- Test-CandyCrushGame/Assets/Scripts/Board.cs
+++ Test-CandyCrushGame/Assets/Scripts/Board.cs
@@ -8,15 +8,32 @@ public class Board : MonoBehaviour
     [SerializeField] private int m_height;
     private BackgroundTile[,] m_tiles;
 
+
+    public GameObject TilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
        m_tiles = new BackgroundTile[m_width, m_height]; 
+       SetUp();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SetUp(){
+        for (int i = 0; i < m_width; i++)
+        {
+            for (int j = 0; j < m_height; j++) 
+            {
+                Vector2 tempPosition = new Vector2(i, j);
+                GameObject backGroundTile = Instantiate(TilePrefab, tempPosition, Quaternion.identity) as GameObject;
+                backGroundTile.transform.parent = transform;
+                backGroundTile.name = "( " + i + "," + j + ")";
+            }
+        }
     }
 }
