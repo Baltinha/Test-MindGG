6248722bb0944a8e0c4fcca7346cfd0caabc4439
Baltinha <matheusramoss666@gmail.com>
2025-03-24 22:37:15 -0300

    Uma fase a mais

--- Test-CandyCrushGame/Assets/Scripts/ScoreManager.cs
+++ Test-CandyCrushGame/Assets/Scripts/ScoreManager.cs
@@ -2,22 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     [Header("Points")]
     [SerializeField] TextMeshProUGUI m_scoreUI;
+    [SerializeField] TextMeshProUGUI m_nextFaseUI;
     [SerializeField] private int m_score;
-    [SerializeField] int m_nexlevel = 2000;
+    [Header("NextLevel")]
+    [SerializeField] string m_levelName;
+    [SerializeField] int m_nexlevel = 0;
+    [SerializeField] GameObject m_nextFase;
 
-    public int Score { get => m_score; set => m_score = value; } 
+    public int Score { get => m_score; set => m_score = value; }
 
+    private void Start()
+    {
+        m_nextFaseUI.text = m_nexlevel.ToString();
+    }
 
     // Update is called once per frame
     void Update()
     {
         m_scoreUI.text = m_score.ToString();
+
+        if (m_score >= m_nexlevel)
+        {
+            m_nextFase.gameObject.SetActive(true);
+        }
     }
 
     public void IncreaseScore(int AmountToIncrise) 
@@ -27,9 +41,6 @@ public class ScoreManager : MonoBehaviour
 
     public void NextLevel()
     {
-        if (m_score >= m_nexlevel)
-        {
-            Debug.Log("Proxima Fase");
-        }
+        SceneManager.LoadScene(m_levelName);
     }
 }
